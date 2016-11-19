cc98595cd896010bf01b218d82992ad3b8638121
Andrei <Andrei@ANDREI-PC>
2016-11-20 04:30:59 +0500

    Populate TestStatus table.

---
+++ TestGenerator/Persistence/Migrations/201611192321304_PopulateTestStatusTable.cs
@@ -0,0 +1,21 @@
+﻿namespace TestGenerator.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateTestStatusTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO TestStatus (Id, Name) VALUES (1, 'Опубликовано')");
+            Sql("INSERT INTO TestStatus (Id, Name) VALUES (2, 'В черновике')");
+            Sql("INSERT INTO TestStatus (Id, Name) VALUES (3, 'В ожидании публикации')");
+            Sql("INSERT INTO TestStatus (Id, Name) VALUES (4, 'Завершено')");
+            Sql("INSERT INTO TestStatus (Id, Name) VALUES (5, 'В архиве')");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM TestStatus WHERE Id IN (1, 2, 3, 4, 5)");
+        }
+    }
+}
