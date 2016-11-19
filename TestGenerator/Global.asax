997771f9ee400646c29192d7acbcc363e2f88d7a
Andrei <Andrei@ANDREI-PC>
2016-11-19 23:48:42 +0500

    Add project files.

---
+++ TestGenerator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestGenerator.MvcApplication" Language="C#" %>
---
+++ TestGenerator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestGenerator.Startup))]
+namespace TestGenerator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
